ce6901eda97cdfff6e780a4d213b4fb92a45c51c
alddaniel <darruspe@me.com>
2025-09-10 14:42:28 -0300

    Atualização automática em 10/09/2025 14:42:28

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import type { View, Company, User, Role, Notification } from '../types';
 import { VIEWS } from '../constants';
 
